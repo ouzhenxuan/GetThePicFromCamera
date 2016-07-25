feaf49f295d709f8036562146c10a273cca4425f
ouzhenxuan <442910407@qq.com>
2016-07-25 10:58:14 +0800

    Initial Commit

---
+++ GetThePicFromCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetThePicFromCamera
+//
+//  Created by 区振轩 on 16/7/25.
+//  Copyright © 2016年 区振轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetThePicFromCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetThePicFromCamera
+//
+//  Created by 区振轩 on 16/7/25.
+//  Copyright © 2016年 区振轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
