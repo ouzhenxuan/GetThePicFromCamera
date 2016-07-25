473a6a5d1f1c816707ae53af4a4df85c1a10841d
ouzhenxuan <442910407@qq.com>
2016-07-25 15:14:14 +0800

    打开相机，获取权限

--- GetThePicFromCamera/ViewController.m
+++ GetThePicFromCamera/ViewController.m
@@ -7,8 +7,28 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AVKit/AVKit.h>
 
-@interface ViewController ()
+
+@interface ViewController ()<UIImagePickerControllerDelegate>
+
+@property (nonatomic, strong)       AVCaptureSession            * session;
+//AVCaptureSession对象来执行输入设备和输出设备之间的数据传递
+@property (nonatomic, strong)       AVCaptureDeviceInput        * videoInput;
+//AVCaptureDeviceInput对象是输入流
+@property (nonatomic, strong)       AVCaptureStillImageOutput   * stillImageOutput;
+//照片输出流对象，当然我的照相机只有拍照功能，所以只需要这个对象就够了
+@property (nonatomic, strong)       AVCaptureVideoPreviewLayer  * previewLayer;
+//预览图层，来显示照相机拍摄到的画面
+@property (nonatomic, strong)       UIBarButtonItem             * toggleButton;
+//切换前后镜头的按钮
+@property (nonatomic, strong)       UIButton                    * shutterButton;
+//拍照按钮
+@property (nonatomic, strong)       UIView                      * cameraShowView;
+//放置预览图层的View
+
+@property (nonatomic,assign) BOOL cameraAvaible;
 
 @end
 
@@ -17,11 +37,193 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.cameraShowView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+    
+    self.cameraShowView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:_cameraShowView];
+    
+    UIButton * btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    btn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:btn];
+//    [btn addTarget:self action:@selector(hehe) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self initialSession];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)hehe{
+    UIImagePickerController *vc = [[UIImagePickerController alloc] init];
+    
+    vc.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    
+    vc.delegate = self;
+    
+    
+    [self presentViewController:vc animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    NSLog(@"123");
+}
+
+
+- (void)isSuppostCamera
+{
+    // 首先查看当前设备是否支持拍照
+//    PHAuthorizationStatus photoAuthorStatus = [PHPhotoLibrary authorizationStatus];
+    
+    AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    if(authStatus == AVAuthorizationStatusRestricted || authStatus ==AVAuthorizationStatusDenied){
+        
+        NSLog(@"没有访问权限");
+        _cameraAvaible = NO;
+    }else{
+        _cameraAvaible = YES;
+    }
+
+//    NSString *mediaType = AVMediaTypeVideo; // Or AVMediaTypeAudio
+//    [AVCaptureDevice requestAccessForMediaType:mediaType completionHandler:^(BOOL granted) {
+//        if(granted){
+//            NSLog(@"Granted access to %@", mediaType);
+//        }
+//        else {
+//            NSLog(@"Not granted access to %@", mediaType);
+//        }
+//    }];
+    
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [self isSuppostCamera];
+    [self setUpCameraLayer];
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    if (self.session) {
+        [self.session startRunning];
+    }
+}
+
+- (void) viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear: animated];
+    if (self.session) {
+        [self.session stopRunning];
+    }
+}
+
+- (void) initialSession
+{
+    //这个方法的执行我放在init方法里了
+    self.session = [[AVCaptureSession alloc] init];
+    self.videoInput = [[AVCaptureDeviceInput alloc] initWithDevice:[self backCamera] error:nil];
+    //[self fronCamera]方法会返回一个AVCaptureDevice对象，因为我初始化时是采用前摄像头，所以这么写，具体的实现方法后面会介绍
+    self.stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    NSDictionary * outputSettings = [[NSDictionary alloc] initWithObjectsAndKeys:AVVideoCodecJPEG,AVVideoCodecKey, nil];
+    //这是输出流的设置参数AVVideoCodecJPEG参数表示以JPEG的图片格式输出图片
+    [self.stillImageOutput setOutputSettings:outputSettings];
+    
+    if ([self.session canAddInput:self.videoInput]) {
+        [self.session addInput:self.videoInput];
+    }
+    if ([self.session canAddOutput:self.stillImageOutput]) {
+        [self.session addOutput:self.stillImageOutput];
+    }
+    
+}
+
+//这是获取前后摄像头对象的方法
+- (AVCaptureDevice *)cameraWithPosition:(AVCaptureDevicePosition) position {
+    NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for (AVCaptureDevice *device in devices) {
+        if ([device position] == position) {
+            return device;
+        }
+    }
+    return nil;
+}
+
+//接下来在viewWillAppear方法里执行加载预览图层的方法
+- (void) setUpCameraLayer
+{
+    if (_cameraAvaible == NO) return;
+    
+    if (self.previewLayer == nil) {
+        self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.session];
+        UIView * view = self.cameraShowView;
+        CALayer * viewLayer = [view layer];
+        [viewLayer setMasksToBounds:YES];
+        
+        CGRect bounds = [view bounds];
+        [self.previewLayer setFrame:bounds];
+        [self.previewLayer setVideoGravity:AVLayerVideoGravityResizeAspect];
+        
+        [viewLayer insertSublayer:self.previewLayer below:[[viewLayer sublayers] objectAtIndex:0]];
+        
+    }
+}
+
+
+//接着我们就来实现切换前后镜头的按钮，按钮的创建我就不多说了
+- (void)toggleCamera {
+    NSUInteger cameraCount = [[AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo] count];
+    if (cameraCount > 1) {
+        NSError *error;
+        AVCaptureDeviceInput *newVideoInput;
+        AVCaptureDevicePosition position = [[_videoInput device] position];
+        
+        if (position == AVCaptureDevicePositionBack)
+            newVideoInput = [[AVCaptureDeviceInput alloc] initWithDevice:[self frontCamera] error:&error];
+        else if (position == AVCaptureDevicePositionFront)
+            newVideoInput = [[AVCaptureDeviceInput alloc] initWithDevice:[self backCamera] error:&error];
+        else
+            return;
+        
+        if (newVideoInput != nil) {
+            [self.session beginConfiguration];
+            [self.session removeInput:self.videoInput];
+            if ([self.session canAddInput:newVideoInput]) {
+                [self.session addInput:newVideoInput];
+                [self setVideoInput:newVideoInput];
+            } else {
+                [self.session addInput:self.videoInput];
+            }
+            [self.session commitConfiguration];
+        } else if (error) {
+            NSLog(@"toggle carema failed, error = %@", error);
+        }
+    }
+}
+
+//这是切换镜头的按钮方法
+- (void) shutterCamera
+{
+    AVCaptureConnection * videoConnection = [self.stillImageOutput connectionWithMediaType:AVMediaTypeVideo];
+    if (!videoConnection) {
+        NSLog(@"take photo failed!");
+        return;
+    }
+    
+    [self.stillImageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+        if (imageDataSampleBuffer == NULL) {
+            return;
+        }
+        NSData * imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+        UIImage * image = [UIImage imageWithData:imageData];
+        NSLog(@"image size = %@",NSStringFromCGSize(image.size));
+    }];
+}
+
+- (AVCaptureDevice *)frontCamera {
+    return [self cameraWithPosition:AVCaptureDevicePositionFront];
+}
+
+- (AVCaptureDevice *)backCamera {
+    return [self cameraWithPosition:AVCaptureDevicePositionBack];
 }
 
 @end
